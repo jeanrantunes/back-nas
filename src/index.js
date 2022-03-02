2c26fa80d07bc501fa5a9f844f6f414e9d970f2e
Jean Antunes <jean.antunes@loft.com.br>
2022-03-02 10:40:22 -0300

    fix: 🐛 deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import 'babel-polyfill'
 import app from 'server'
 import { PORT } from 'config'
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 export default app
